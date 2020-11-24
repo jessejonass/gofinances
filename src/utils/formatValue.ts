a553ae439b25bf0f1038becf8b2eebfecf2e0f4f
Jesse Jonas <jessejonas13@gmail.com>
2020-11-24 10:31:15 -0300

    finalizando o front end

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,7 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value); // Passar parâmetros para passar o formato que deve ser retornado
 
 export default formatValue;
